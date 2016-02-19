1c983b1b9afd3805a560ee88ce8363663c72b9d4
Sudhanva Naryana <nsudhanva@gmail.com>
2016-02-19 16:22:06 +0530

    UI for Login Activity

---
+++ app/src/main/java/sudhanva/narayana/cryptick/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package sudhanva.narayana.cryptick.login;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sudhanva.narayana.cryptick.R;
+
+/**
+ * Created by nsudh on 19-02-2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+    }
+}
